0b7cab70b00348b401500d01ed9273d35d88a2e3
kacperkaczor97 <48179633+kacperkaczor97@users.noreply.github.com>
2021-01-28 13:54:57 +0100

    Update Cifar10.py
    Wersja ostateczna

--- Lab6/Cifar10.py
+++ Lab6/Cifar10.py
@@ -2,9 +2,7 @@
 Autorzy:
 Karol Niemykin
 Kacper Kaczor
-
-Celem zadania jest nauczy program rozpoznawania zwierzat poprzez siec neuronowa ze zbioru CIFAR-10
-
+Celem zadania jest nauczy program rozpoznawania liczb poprzez siec neuronowa ze zbioru Mnist
 Uruchomienie:
   - Pobrac projekt z Github
   - Stworzyc projekt w PyCharm
@@ -20,28 +18,35 @@ import matplotlib.pyplot as plt
 '''
 Importowanie danych z biblioteki TensorFlow
 '''
-cifar10 = tf.keras.datasets.cifar10
+mnist = tf.keras.datasets.mnist
 
-# (train_images, train_labels), (test_images, test_labels)
-(x_train, y_train), (x_test, y_test)= cifar10.load_data()
+(x_train, y_train), (x_test, y_test)= mnist.load_data()
 '''
 Przypisanie etykiet do obrazkow
 '''
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
-               'dog', 'frog', 'horse', 'ship', 'truck']
+class_names = ['0', '1', '2', '3', '4',
+               '5', '6', '7', '8', '9']
 
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[y_train[i]])
+plt.show()
 
 '''
 Przeksztalcenie formatow obrazkow
 '''
-x_train = x_train.reshape(x_train.shape[0], 32, 32, 3)
-x_test = x_test.reshape(y_test.shape[0], 32, 32, 3)
-
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(y_test.shape[0], 28, 28, 1)
 '''
 Definiowanie modelu do konstruktora 2D
 '''
 model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32,32,3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
         tf.keras.layers.MaxPooling2D(2,2),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
