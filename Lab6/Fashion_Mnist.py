815743fce7dc785e761e9507f3b61bd8183d7a49
kacperkaczor97 <48179633+kacperkaczor97@users.noreply.github.com>
2021-01-28 11:48:25 +0100

    Update Fashion_Mnist.py

--- Lab6/Fashion_Mnist.py
+++ Lab6/Fashion_Mnist.py
@@ -1,20 +1,35 @@
-# TensorFlow and tf.keras
-import tensorflow as tf
+'''
+Autorzy:
+Karol Niemykin
+Kacper Kaczor
+Celem zadania jest nauczy program rozpoznawania zwierzat poprzez siec neuronowa ze zbioru CIFAR-10
+Uruchomienie:
+  - Pobrac projekt z Github
+  - Stworzyc projekt w PyCharm
+  - Zimportowac biblioteki i uruchomic program
+'''
 
-# Helper libraries
+'''
+Importowanie potrzebnych bibliotek
+'''
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-print(tf.__version__)
-
+'''
+Importowanie danych z biblioteki TensorFlow
+'''
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
+'''
+Przypisanie etykiet do obrazkow
+'''
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-
+'''
+Przeksztalcenie formatow obrazkow
+'''
 plt.figure(figsize=(10,10))
 for i in range(25):
     plt.subplot(5,5,i+1)
@@ -24,11 +39,15 @@ for i in range(25):
     plt.xlabel(class_names[train_labels[i]])
 plt.show()
 
-# reshaping
+'''
+Przeksztalcenie formatow obrazkow
+'''
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
 test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
-# define the model
+'''
+Definiowanie modelu do konstruktora 2D
+'''
 model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
         tf.keras.layers.MaxPooling2D(2,2),
@@ -36,12 +55,18 @@ model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dense(10, activation='softmax')
         ])
-
+'''
+Kompilacja i optymalizacja modelu
+'''
 model.compile(optimizer='adam', loss="sparse_categorical_crossentropy",
               metrics=['accuracy'])
 
-# train
+'''
+Wytrenowanie modelu
+'''
 model.fit(train_images, train_labels, epochs=10)
 test_loss, test_acc = model.evaluate(train_images, train_labels, verbose=2)
-
+'''
+Wypisanie dokladnosci uczenia
+'''
 print("Test accuracy: ", test_acc)
